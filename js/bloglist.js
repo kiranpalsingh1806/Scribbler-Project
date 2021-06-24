791d64700f104e75405352b5a426868fd5bf8534
kiranpalsingh1806 <kiranpalsingh1806@gmail.com>
2021-06-24 22:38:55 +0530

    Added Post page with delete option

---
+++ js/bloglist.js
@@ -0,0 +1,25 @@
+
+function myFunction1() {
+  var myobj = document.getElementById("demo1");
+  myobj.remove();
+}
+
+function myFunction2() {
+  var myobj = document.getElementById("demo2");
+  myobj.remove();
+}
+
+function myFunction3() {
+  var myobj = document.getElementById("demo3");
+  myobj.remove();
+}
+
+function myFunction4() {
+  var myobj = document.getElementById("demo4");
+  myobj.remove();
+}
+
+function myFunction5() {
+  var myobj = document.getElementById("demo5");
+  myobj.remove();
+}
