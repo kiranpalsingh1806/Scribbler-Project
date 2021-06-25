fe47202ed143e6ae4cdaeed59f4a15308958068b
kiranpalsingh1806 <kiranpalsingh1806@gmail.com>
2021-06-25 17:05:07 +0530

    Added modal for deleting post

--- js/bloglist.js
+++ js/bloglist.js
@@ -1,24 +1,59 @@
 
+
+// Get the modal
+var modal = document.getElementById('id01');
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 function myFunction1() {
   var myobj = document.getElementById("demo1");
   myobj.remove();
 }
 
+
+
+var modal2 = document.getElementById('id02');
+window.onclick = function(event) {
+  if (event.target == modal2) {
+    modal2.style.display = "none";
+  }
+}
 function myFunction2() {
   var myobj = document.getElementById("demo2");
   myobj.remove();
 }
 
+
+var modal3 = document.getElementById('id03');
+window.onclick = function(event) {
+  if (event.target == modal2) {
+    modal3.style.display = "none";
+  }
+}
 function myFunction3() {
   var myobj = document.getElementById("demo3");
   myobj.remove();
 }
 
+var modal4 = document.getElementById('id02');
+window.onclick = function(event) {
+  if (event.target == modal4) {
+    modal4.style.display = "none";
+  }
+}
 function myFunction4() {
   var myobj = document.getElementById("demo4");
   myobj.remove();
 }
 
+var modal5 = document.getElementById('id05');
+window.onclick = function(event) {
+  if (event.target == modal2) {
+    modal5.style.display = "none";
+  }
+}
 function myFunction5() {
   var myobj = document.getElementById("demo5");
   myobj.remove();
